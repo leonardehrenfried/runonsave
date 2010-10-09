39ffe7a87297e644590ceaf027aec5a7db1d44c7
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2010-10-09 17:45:45 +0200

    command to execute not hard coded in anymore

--- runonsave.py
+++ runonsave.py
@@ -22,10 +22,7 @@ AUTHOR
 
 """
 
-import os
-import subprocess
-import time
-import re
+import os, sys, subprocess, time
 
 SCAN_INTERVAL=10
 EXCLUDES=['.git', '.hg', 'CVS']
@@ -37,14 +34,20 @@ def main ():
     last_modified=get_last_modified(cwd)
     if (last_modified > last_run):
       print "*** Save detected - running command ***"
-      subprocess.Popen(['pdflatex', 'cv.tex', ])
+      args=sys.argv
+      
+      #strip out the python command
+      if args[0] is "python":
+        args=args[1:]
+      args=args[1:]
+
+      subprocess.Popen(args)
       last_run=last_modified
     time.sleep(SCAN_INTERVAL)
 
 def get_last_modified(current_dir):
     """Recurses though a directory tree and finds the newest last modified date"""
     last_modified=os.path.getmtime(current_dir)
-    #print "checking %s: " % current_dir
     subdirs=os.listdir(current_dir)
     subdirs = [subdir for subdir in subdirs if should_check(subdir)]
 
@@ -56,7 +59,7 @@ def get_last_modified(current_dir):
     return last_modified
 
 def should_check(path):
-  """Checks if path should be recursively searched. Exludes files and SCM dirs"""
+  """Checks if path should be recursively searched. Excludes files and SCM dirs"""
   if(os.path.isdir(path)):
     basename=os.path.basename(path)
     if basename not in EXCLUDES:
